b8d3c7b397aaeb44bb241c8e9c69b2b11e4b75dd
Roman Sulaymanov <92111647+DriverOnLips@users.noreply.github.com>
2024-05-16 09:19:18 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/kts-gastronaut/sw.js', { scope: '/kts-gastronaut/' })})}
