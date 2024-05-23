46092ca6792f64e87a98c7a8d366e2bb4bff9f46
Roman Sulaymanov <92111647+DriverOnLips@users.noreply.github.com>
2024-05-23 21:40:14 +0300

    commented unused code

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable react/react-in-jsx-scope */
 import cn from 'classnames';
 import { observer } from 'mobx-react-lite';
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import logo from 'assets/svg/logo.svg';
-import Filters from 'components/Filters/Filters';
+// import Filters from 'components/Filters/Filters';
 import Menu from 'components/Icons/Menu/Menu';
 import MenuClose from 'components/Icons/MenuClose/MenuClose';
-import { useLocalStore } from 'hooks/useLocalStore';
-import RecipeListStore from 'stores/RecipeListStore/RecipeListStore';
+// import { useLocalStore } from 'hooks/useLocalStore';
+// import RecipeListStore from 'stores/RecipeListStore/RecipeListStore';
 import rootStore from 'stores/RootStore/instance';
-import { RecipeFromListModel } from 'types/RecipeFromList/RecipeFromList';
+// import { RecipeFromListModel } from 'types/RecipeFromList/RecipeFromList';
 import styles from './Header.module.scss';
 
 const Header = () => {
@@ -47,60 +47,60 @@ const Header = () => {
 	);
 
 	// filers
-	const params = useMemo(
-		() => new URLSearchParams(window.location.search),
-		[window.location.search],
-	);
-	const page = +(params.get('page') || 1);
+	// const params = useMemo(
+	// 	() => new URLSearchParams(window.location.search),
+	// 	[window.location.search],
+	// );
+	// const page = +(params.get('page') || 1);
 
-	const recipeListStore = useLocalStore(() => new RecipeListStore());
+	// const recipeListStore = useLocalStore(() => new RecipeListStore());
 
-	const { inputStore, dropdownStore, recipeList, getRecipes } = recipeListStore;
+	// const { inputStore, dropdownStore, recipeList, getRecipes } = recipeListStore;
 
-	const [, setItems] = useState<RecipeFromListModel[]>(recipeList);
+	// const [, setItems] = useState<RecipeFromListModel[]>(recipeList);
 
-	const loadRecipes = useCallback(
-		(pg?: number) => {
-			getRecipes({
-				count: 100,
-				page: pg || page,
-				query: params.get('query') || null,
-				type: params.get('type') || null,
-			});
-		},
-		[params, page, getRecipes],
-	);
+	// const loadRecipes = useCallback(
+	// 	(pg?: number) => {
+	// 		getRecipes({
+	// 			count: 100,
+	// 			page: pg || page,
+	// 			query: params.get('query') || null,
+	// 			type: params.get('type') || null,
+	// 		});
+	// 	},
+	// 	[params, page, getRecipes],
+	// );
 
-	const onButtonClick = useCallback(() => {
-		const newSearchParams = new URLSearchParams(window.location.search);
-		newSearchParams.set('page', '1');
-		navigate(`?${newSearchParams.toString()}`, { replace: true });
+	// const onButtonClick = useCallback(() => {
+	// 	const newSearchParams = new URLSearchParams(window.location.search);
+	// 	newSearchParams.set('page', '1');
+	// 	navigate(`?${newSearchParams.toString()}`, { replace: true });
 
-		setItems([]);
-		loadRecipes(1);
-	}, [navigate, loadRecipes]);
+	// 	setItems([]);
+	// 	loadRecipes(1);
+	// }, [navigate, loadRecipes]);
 
-	const onInputChange = useCallback(
-		(value: string) => {
-			inputStore.setValue(value);
+	// const onInputChange = useCallback(
+	// 	(value: string) => {
+	// 		inputStore.setValue(value);
 
-			const newSearchParams = new URLSearchParams(window.location.search);
-			newSearchParams.set('query', value);
-			navigate(`?${newSearchParams.toString()}`, { replace: true });
-		},
-		[inputStore, navigate],
-	);
+	// 		const newSearchParams = new URLSearchParams(window.location.search);
+	// 		newSearchParams.set('query', value);
+	// 		navigate(`?${newSearchParams.toString()}`, { replace: true });
+	// 	},
+	// 	[inputStore, navigate],
+	// );
 
-	const onMultiDropdownClick = useCallback(
-		(value: string) => {
-			const newSearchParams = new URLSearchParams(window.location.search);
-			value !== 'Choose a category'
-				? newSearchParams.set('type', value)
-				: newSearchParams.set('type', '');
-			navigate(`?${newSearchParams.toString()}`, { replace: true });
-		},
-		[navigate],
-	);
+	// const onMultiDropdownClick = useCallback(
+	// 	(value: string) => {
+	// 		const newSearchParams = new URLSearchParams(window.location.search);
+	// 		value !== 'Choose a category'
+	// 			? newSearchParams.set('type', value)
+	// 			: newSearchParams.set('type', '');
+	// 		navigate(`?${newSearchParams.toString()}`, { replace: true });
+	// 	},
+	// 	[navigate],
+	// );
 
 	return (
 		<nav className={styles.navbar_desctop}>
